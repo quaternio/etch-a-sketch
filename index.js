604b65dd321c9439654a1696216bfc194cfc5dd9
Thomas Noel <thomas.c.noel@gmail.com>
2023-09-03 14:59:48 -0700

    Clean up pixel borders; add grid size changing and clear buttons

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const container = document.querySelector(".container");
+const gridButton = document.querySelector(".grid-button");
+const clearButton = document.querySelector(".clear-button");
+let gridDim = 16;
 
 const gridCreator = (width, height) => {
   container.innerHTML = "";
@@ -17,12 +20,18 @@ const gridCreator = (width, height) => {
       let pixel = document.createElement("div");
       pixel.classList.add(`col-${i}`);
       pixel.classList.add(`row-${j}`);
-      pixel.style.borderStyle = "solid";
+
+      pixel.style.borderLeftStyle = "solid";
+      pixel.style.borderTopStyle = "solid";
+      if (i === width - 1) pixel.style.borderRightStyle = "solid";
+      if (j === height - 1) pixel.style.borderBottomStyle = "solid";
+
       pixel.style.borderColor = "black";
       pixel.style.flex = "auto";
 
-      pixel.addEventListener('mouseover', (e) => {
+      pixel.addEventListener('mouseenter', (e) => {
         pixel.style.backgroundColor = "grey"; 
+        console.log(`Entered pixel ${j}, ${i}`);
       });
 
       subcontainer.appendChild(pixel);
@@ -30,4 +39,22 @@ const gridCreator = (width, height) => {
   }  
 }
 
-gridCreator(32,32);
+// Set grid button callback
+gridButton.onclick = () => {
+  let newGridSize = Number(prompt("New Grid Size (1 to 100): "));
+  gridDim = newGridSize;
+  
+  if (newGridSize > 0 && newGridSize < 101) {
+    gridCreator(gridDim, gridDim);
+  } else {
+    alert("Please enter a grid size between 1 and 100"); 
+  }
+}
+
+// Set clear button callback
+clearButton.onclick = () => {
+  gridCreator(gridDim, gridDim);
+}
+
+// Generate grid
+gridCreator(gridDim, gridDim);
